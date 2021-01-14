8a8cadf2ea57ac1cbb2df1c004f82f5f6ec80b4e
idogrf <idogreenfeld@gmail.com>
2021-01-14 22:16:25 +0200

    refactor

--- refresh_torrents_rss.py
+++ refresh_torrents_rss.py
@@ -2,6 +2,5 @@ from torrent_tools.data_operations import delete_small_dirs
 from torrent_tools.torrent_operations import refresh_rss_torrents
 
 
-if __name__ == '__main__':
-    refresh_rss_torrents()
-    delete_small_dirs()
+refresh_rss_torrents()
+delete_small_dirs()
